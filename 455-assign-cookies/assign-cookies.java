93d8ba0260edc59922e4030996e79b7df0ec1434
Sneha Tiwari <snehat1806@gmail.com>
2026-08-09 05:31:38 +0530

    Time: 14 ms (91.99%) | Memory: 51.6 MB (37.60%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n=g.length;
+        int m=s.length;
+        int count=0;
+        int i=0;
+        int j=0;
+        while(i<n&&j<m){
+        if(s[j]>=g[i]){
+            i++;
+            j++;
+            count++;
+        }else{
+            j++;
+            }
+               }
+                     return count;
+    }
+}
