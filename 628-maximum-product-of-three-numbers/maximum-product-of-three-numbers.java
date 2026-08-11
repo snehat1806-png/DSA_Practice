17c8ab4d3a47848d7901e07e35f10a8a1413a777
Sneha Tiwari <snehat1806@gmail.com>
2026-08-11 06:33:57 +0530

    Time: 15 ms (67.48%) | Memory: 48.1 MB (47.98%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,44 +1,44 @@
-class Solution {
-    public int maximumProduct(int[] nums) {
-        int max1 = Integer.MIN_VALUE;
-        int max2 = Integer.MIN_VALUE;
-        int max3 = Integer.MIN_VALUE;
-
-        int min1 = Integer.MAX_VALUE;
-        int min2 = Integer.MAX_VALUE;
-
-        for (int x : nums) {
-            if (x > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = x;
-            } else if (x > max2) {
-                max3 = max2;
-                max2 = x;
-            } else if (x > max3) {
-                max3 = x;
-            }
-
-            if (x < min1) {
-                min2 = min1;
-                min1 = x;
-            } else if (x < min2) {
-                min2 = x;
-            }
-        }
-
-        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
-    }
-}
 // class Solution {
 //     public int maximumProduct(int[] nums) {
-//         Arrays.sort(nums);
+//         int max1 = Integer.MIN_VALUE;
+//         int max2 = Integer.MIN_VALUE;
+//         int max3 = Integer.MIN_VALUE;
 
-//         int n = nums.length;
+//         int min1 = Integer.MAX_VALUE;
+//         int min2 = Integer.MAX_VALUE;
 
-//         int product1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
-//         int product2 = nums[0] * nums[1] * nums[n - 1];
+//         for (int x : nums) {
+//             if (x > max1) {
+//                 max3 = max2;
+//                 max2 = max1;
+//                 max1 = x;
+//             } else if (x > max2) {
+//                 max3 = max2;
+//                 max2 = x;
+//             } else if (x > max3) {
+//                 max3 = x;
+//             }
 
-//         return Math.max(product1, product2);
+//             if (x < min1) {
+//                 min2 = min1;
+//                 min1 = x;
+//             } else if (x < min2) {
+//                 min2 = x;
+//             }
+//         }
+
+//         return Math.max(max1 * max2 * max3, min1 * min2 * max1);
 //     }
 // }
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        int product1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+        int product2 = nums[0] * nums[1] * nums[n - 1];
+
+        return Math.max(product1, product2);
+    }
+}
