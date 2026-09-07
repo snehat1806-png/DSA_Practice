b0a2416317b9caea911bdf3c4750b0a82ec428e6
Sneha Tiwari <snehat1806@gmail.com>
2026-09-07 08:02:47 +0530

    Time: 1 ms (96.74%) | Memory: 47.1 MB (98.44%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,55 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-    //    int m = matrix.length;
-    //     int n = matrix[0].length;
-    //     // Step 1: Find all 0s
-    //     for (int i = 0; i < m; i++) {
-    //         for (int j = 0; j < n; j++) {
-    //             if (matrix[i][j] == 0) {
-    //                 // Make entire row -1
-    //                 for (int col = 0; col < n; col++) {
-    //                     if (matrix[i][col] != 0) {
-    //                         matrix[i][col] = -1;
-    //                     }
-    //                 }
+       int m=matrix.length;
+       int n=matrix[0].length;
 
-    //                 // Make entire column -1
-    //                 for (int row = 0; row < m; row++) {
-    //                     if (matrix[row][j] != 0) {
-    //                         matrix[row][j] = -1;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     // Step 2: Convert all -1 into 0
-    //     for (int i = 0; i < m; i++) {
-    //         for (int j = 0; j < n; j++) {
-    //             if (matrix[i][j] == -1) {
-    //                 matrix[i][j] = 0;
-    //             }
-    //         }
-    //     }
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        int[] row = new int[m];
-        int[] col = new int[n];
-
-        // Step 1: Find all 0s
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+        int []row=new int[m];
+        int[]col=new int[n];
+        //find alll 0's in the matrix
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                 row[i]=1;
+                 col[j]=1;   
                 }
             }
         }
-        // Step 2: Make marked rows and columns 0
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i] == 1 || col[j] == 1) {
-                    matrix[i][j] = 0;
+        //after finding alll 0's we convert all rows and col to 0 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]==1||col[j]==1){
+                    matrix[i][j]=0;
                 }
             }
         }
